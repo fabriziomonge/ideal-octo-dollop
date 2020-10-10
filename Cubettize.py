5a3b88067ef374a24b2308bc5c4141e171ed3369
Fabrizio Monge <37080465+fabriziomonge@users.noreply.github.com>
2020-10-10 20:21:27 +0200

    Add files via upload

---
+++ Cubettize.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image
+
+st.title("Kubettize App for Pyssla")
+
+image = Image.open('KUBETTIZE.png')
+st.sidebar.image(image, use_column_width=True)
+
+uploaded_file1 = st.sidebar.file_uploader("Carica l'immagine da cubettizare", type=["jpg"])
+
+
+if uploaded_file1 is not None:
+    # Open Paddington
+    st.write('''### Immagine originale''')
+    img = Image.open(uploaded_file1)
+    st.image(img)
+    # Resize smoothly down to 16x16 pixels
+
+    cube = st.sidebar.slider('definizione', 20,60)
+
+    imgSmall = img.resize((cube,cube),resample=Image.BILINEAR)
+
+    # Scale back up using NEAREST to original size
+    result = imgSmall.resize(img.size,Image.NEAREST)
+
+    # Save
+    
+#     fig = plt.imshow(result)
+    st.write('''### Immagine modificata''')
+    st.image(result)
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
